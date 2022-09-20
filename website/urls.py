315c70e7395b225193ba78502393ce38703cc5b2
WillianBox <willianf4m@gmail.com>
2022-09-19 22:44:18 -0300

    Aula 2

--- website/urls.py
+++ website/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from hello_world import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # URL configurada
+    path('', views.index, name="homepage")
 ]
